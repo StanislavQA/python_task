5613f31b34dd61bea09b53a7646c1746e05c3f03
StanislavQA <ssv0123456@gmail.com>
2017-08-28 16:07:01 +0300

    No verification

--- timeweb2.py
+++ timeweb2.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
 
 def is_alert_present(wd):
     try:
@@ -16,12 +15,13 @@ class timeweb2(unittest.TestCase):
         self.wd.implicitly_wait(60)
     
     def test_timeweb2(self):
-        success = True
         wd = self.wd
+        # Open home page
         wd.get("https://timeweb.com/ru/")
         wd.find_element_by_link_text("ХОСТИНГ").click()
         wd.find_element_by_link_text("РАЗМЕСТИТЬ САЙТ").click()
         wd.find_element_by_css_selector("li.item.selected").click()
+        # login
         wd.find_element_by_xpath("//div[@class='overlay']/div/div/div[14]/form/div[2]/div[1]/div[2]/div[2]/input").click()
         wd.find_element_by_xpath("//div[@class='overlay']/div/div/div[14]/form/div[2]/div[1]/div[2]/div[2]/input").clear()
         wd.find_element_by_xpath("//div[@class='overlay']/div/div/div[14]/form/div[2]/div[1]/div[2]/div[2]/input").send_keys("Иванов Петр Иванович")
@@ -32,11 +32,12 @@ class timeweb2(unittest.TestCase):
         if not wd.find_element_by_id("c4").is_selected():
             wd.find_element_by_id("c4").click()
         wd.find_element_by_link_text("ЗАКАЗАТЬ").click()
-        wd.find_element_by_xpath("//div[2]/div[1]/div[1]/button").click
+
 
     def assert_check(self):
+        # Затык в проверке, дальше наброски кода без готового решения
         wd.find_element_by_link_text("Year+").text()
-        assert "Year" in self.driver.title //проверка на соответствие
+        assert "Year+" in self.driver.title # проверка на соответствие
         wd.find_element_by_id("ui-id-4").click()
         self.assertTrue(success)
     
